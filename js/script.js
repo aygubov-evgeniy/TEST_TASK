e1f7cba91c668f980a413c977abcb804c0d59c51
gitZhekos <aigubov.zheka@yandex.ru>
2019-03-15 20:38:32 +0200

    init project

---
+++ js/script.js
@@ -0,0 +1,13 @@
+var orderApp = (function() {
+  return{
+    init: function() {
+      this.countProducts();
+    },
+
+    countProducts: function() {
+      
+    }
+  }
+}());
+
+orderApp.init();
